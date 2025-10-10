9a9db32c87ce1380dd928ca4c04f64052b6530bc
xiaoheiCat <104667776+xiaoheiCat@users.noreply.github.com>
2025-10-10 17:01:15 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,33 @@
 #! /bin/bash
 
+echo "Changing Directory..."
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+echo "Installing Docker..."
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 echo "Installing K3s..."
 curl –sfL \
     https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | \
     INSTALL_K3S_MIRROR=cn sh -s - \
     --system-default-registry "registry.cn-hangzhou.aliyuncs.com" --disable=traefik
+    
+echo "Configuring K3s..."
+cat /etc/rancher/k3s/k3s.yaml > ./configs/k8s.yml
+sed -i 's/127.0.0.1/172.20.0.1/g' ./configs/k8s.yml
+sudo systemctl stop k3s
+echo "tls-san:" > /etc/rancher/k3s/config.yaml
+echo "  - 172.20.0.1" >> /etc/rancher/k3s/config.yaml
+sudo rm /var/lib/rancher/k3s/server/tls/server-ca.crt
+sudo rm /var/lib/rancher/k3s/server/tls/server-ca.key
+sudo systemctl start k3s
