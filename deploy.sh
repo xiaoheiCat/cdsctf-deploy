cc0e7ada5aeef427a16bc81f2928e45c7cdff710
xiaoheiCat <104667776+xiaoheiCat@users.noreply.github.com>
2026-06-22 18:27:08 +0800

    Fix curl command syntax for K3s installation

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Installing K3s..."
-curl –sfL \
+curl -sfL \
     https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | \
     INSTALL_K3S_MIRROR=cn sh -s - \
     --system-default-registry "registry.cn-hangzhou.aliyuncs.com" --disable=traefik
